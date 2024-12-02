234b8e73ab5f28549aa80e69aee3a97d14e724d9
hernirg <hernanrg85@gmail.com>
2024-12-02 16:20:53 +0100

    Initial Commit

---
+++ EstacionesDeServicio/EstacionesDeServicioApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstacionesDeServicioApp.swift
+//  EstacionesDeServicio
+//
+//  Created by Hernán Rodríguez on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EstacionesDeServicioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
